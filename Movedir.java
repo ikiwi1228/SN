74180b89d5911eafb9a47acbdf1c78e6bd2a7a34
ikiwi1228 <junyi5959@gmail.com>
2019-05-31 08:06:13 -0700

    Add files via upload

---
+++ Movedir.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Snake;
+
+/**
+ *
+ * @author luj0314
+ */
+public interface Movedir {
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
+    
+}
